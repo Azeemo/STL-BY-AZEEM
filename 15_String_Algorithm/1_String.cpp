b0e6ee48e93a81f7ae3c68daa9278fe6441eb582
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-05-17 00:29:50 +0530

    Add files via upload

---
+++ 15_String_Algorithm/1_String.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+string str = "geeksforgeeks";
+cout<<str;
+// printing of string
+
+// ADVANTAGES OVER C SIIZE ARRAY
+// -> we do not have to worry about size
+// -> can assign a string later
+// -> support of operators like +,<,>,==
+// -> richer library functions
+// can be converted to c style if needed
+
+// to GET LENGHT 
+cout<<str.length()<<endl;
+
+// WE CAN ADD STRINGS AS
+str = str + "xyz";
+cout<<str<<endl;
+
+// to find the substring
+cout<<str.substr(1,3)<<endl; 
+// both index are included
+
+
+// to check whether the substring is present or not
+cout<<str.find("fg");
+// return the first occurence of the string
+// -> here fg is occuring at the first index so : op / -> 1
+
+
+return 0;
+}
