a6bfbbdd9182ad18514927bce7e0840c005e54cd
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-06-11 21:41:53 +0530

    Create 1_Pairs_Intro.cpp

---
+++ 2_Pairs/1_Pairs_Intro.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+pair<int,int>p1{10,20};
+pair<int,string>p2{10,"azeem"};
+  // if we dont initialie the value then their is defalut constructor which intialize value as 0
+  
+  cout<<p1.first<<" "<<p1.second<<endl;
+  cout<<p2.first<<" "<<p2.second<<endl;
+return 0 ;
+}
