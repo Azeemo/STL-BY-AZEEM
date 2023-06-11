4bd1679cd74a3c6eaf1e7aff09aacf72d2a6d66d
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-06-11 21:04:40 +0530

    7_Transpose_Of_Matrix.cpp

---
+++ INTRODUCTION TO STL/7_Transpose_Of_Matrix.cpp
@@ -0,0 +1,9 @@
+vector<vector<int>> ans;
+    for(int i = 0; i < n; i++)
+    {
+        vector<int> temp;
+        for(int j = 0; j < n; j++)
+            temp.push_back(a[j][i]);
+            
+        ans.push_back(temp);
+    }
