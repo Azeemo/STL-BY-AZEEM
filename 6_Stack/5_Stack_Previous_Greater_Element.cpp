a21d4dc041c9b0bf60aba0fb026cdde81db02289
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-06-12 20:59:29 +0530

    Add files via upload

---
+++ 6_Stack/5_Stack_Previous_Greater_Element.cpp
@@ -0,0 +1,25 @@
+
+
+// IMPLEMENTATION IS SAME AS STACK SPAN PROBLEM
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n = 7;
+int price[n] = {100 80 60 70 60 75 85};
+
+vector<int> v;
+stack<int> stk;
+stk.push(0);
+v.push_back(-1);
+for (int i = 1; i < n; ++i)
+{
+	if (stk.empty()==false && stk.top()<=arr[i])
+	{
+	       	
+	}
+}
+
+
+return 0;
+}
