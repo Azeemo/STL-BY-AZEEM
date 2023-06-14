761e24e3d6f306d0760236acbcea23a7c6824653
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-06-14 19:07:50 +0530

    9_Strings_CmpCharWithInteger.cpp

---
+++ 15_String_Algorithm/9_Strings_CmpCharWithInteger.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+  solve(){
+    int n,d;
+    cin>>n>>d;
+    string s;
+    cin>>s;
+    
+  }
+int main()
+{
+ int t;
+  cin>>t;
+  while(t--){
+  solve();
+  }
+  
+return 0 ;
+}
