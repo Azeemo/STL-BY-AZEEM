e8e5b8d7da63ce1489affd42bca3c7941d565f45
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-06-14 19:25:32 +0530

    10_Strings_CmpCharWithInteger.cpp

--- 15_String_Algorithm/10_Strings_CmpCharWithInteger.cpp
+++ 15_String_Algorithm/10_Strings_CmpCharWithInteger.cpp
@@ -1,19 +1,27 @@
-#include<bits/stdc++.h>
-using namespace std;
-  solve(){
-    int n,d;
-    cin>>n>>d;
-    string s;
-    cin>>s;
-    
-  }
-int main()
-{
- int t;
-  cin>>t;
-  while(t--){
-  solve();
-  }
-  
-return 0 ;
+// CODEFORCES 1811 A Problem
+#include<bits/stdc++.h>                     
+using namespace std;                   
+#define ll long long
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n,d;cin>>n>>d;
+        string str1;cin>>str1;
+   
+        int k=1;
+        for(int i=0;i<n;i++){
+            if(str1[i]-'0'>=d){
+                cout<<str1[i];
+            }else{
+                if(k==1){
+                    cout<<d<<str1[i];
+                    k++;
+                }else{
+                    cout<<str1[i];
+                }
+            }
+        }
+        if(k==1) cout<<d;
+        cout<<endl;
+    }
 }
